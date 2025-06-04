2b4a2030a63d0563a46cc1282b0c83f895fa8b5f
Rakibul Islam <rakib.ca@gmail.com>
2025-06-04 00:22:45 -0600

    code update

--- 09_setInterval.js
+++ 09_setInterval.js
@@ -1,22 +1,27 @@
-let secondsRemaining = 3;
-function startCountdown() {
-  if (secondsRemaining === 0) {
-    console.log(`Your device is shutting down`);
-    clearInterval(shutdownTimer);
-  } else if (secondsRemaining > 0) {
-    secondsRemaining--;
-    console.log(`Your device will shut down in ${secondsRemaining} seconds`);
-  }
+/*
+  Challenge:
+  1. Every 1000 milliseconds, log out: 
+      `Your device will shut down in ${secondsRemaining} seconds`.
+      Decrement 'secondsRemaining'.
+  2. When 'secondsRemaining' hits zero, you should log out 
+      `Your device is shutting down`. 
+      At this point, the setInterval should be cleared. What method 
+      can you use to do that? (We used clearTimeout to cancel setTimeout.)
+  */
 
-  /*
-Challenge:
-1. Every 1000 milliseconds, log out: 
-    `Your device will shut down in ${secondsRemaining} seconds`.
-    Decrement 'secondsRemaining'.
-2. When 'secondsRemaining' hits zero, you should log out 
-    `Your device is shutting down`. 
-    At this point, the setInterval should be cleared. What method 
-    can you use to do that? (We used clearTimeout to cancel setTimeout.)
-*/
+function startCountdown(device) {
+  let secondsRemaining = 3;
+
+  const shutdownTimer = setInterval(function () {
+    if (secondsRemaining > 0) {
+      console.log(
+        `Your ${device} will shut down in ${secondsRemaining} seconds`
+      );
+      secondsRemaining--;
+    } else {
+      console.log(`Your ${device} is shutting down`);
+      clearInterval(shutdownTimer);
+    }
+  }, 1000);
 }
-const shutdownTimer = setInterval(startCountdown, 1000);
+startCountdown("MacBook");
